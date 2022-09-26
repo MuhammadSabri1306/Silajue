ffcf45771d5c0f162750f756f5495d1d130a04ee
MuhammadSabri1306 <muhammadsabri1306@gmail.com>
2022-09-26 15:58:38 +0800

    first setup

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js}"],
+  theme: {
+    fontFamily: {
+      "display": ["Josefin Sans", ...defaultTheme.fontFamily.sans],
+      "body": ["Open Sans", ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      colors: {
+        black: "#17252a",
+        white: "#feffff",
+        primary: colors.sky,
+        secondary: colors.purple,
+        danger: colors.rose,
+        warning: colors.yellow
+      }
+    },
+  },
+  plugins: [],
+}
