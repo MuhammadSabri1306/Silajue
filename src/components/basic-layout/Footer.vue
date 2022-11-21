db15bafb98f2ad0325b2927d07e9aca270d9b30c
MuhammadSabri1306 <muhammadsabri1306@gmail.com>
2022-11-21 13:24:18 +0800

    update footer

--- src/components/basic-layout/Footer.vue
+++ src/components/basic-layout/Footer.vue
@@ -8,29 +8,32 @@
 							<span class="text-sm leading-none transition-all ml-0 group-hover:ml-8">Pasar Bibit Ternak Daring</span>
 						</router-link>
 						<p class="text-sm mb-2 text-gray-200">Alamat</p>
-						<p class="text-sm text-gray-200">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fringilla eget euismod netus est accumsan.</p>
+						<p class="text-sm text-gray-200"><a href="https://goo.gl/maps/VNnu4i7HiubN4WLq8" target="_blank">Jl. Veteran Selatan No.234, Kec. Mamajang,<br>Kota Makassar, Sulawesi Selatan 90131</a></p>
 					</div>
 				<div class="mx-auto">
-					<p class="text-sm text-shadow-black text-gray-100">Contact Us</p>
+					<p class="text-sm text-shadow-black text-gray-100">Hubungi Kami</p>
 					<div class="flex items-center my-6 gap-6 text-3xl">
 						<a href="#" class="text-gray-100 hover:text-primary-600 text-shadow-black hover-margin">
 							<font-awesome-icon icon="fa-brands fa-facebook" />
 						</a>
-						<a href="#" class="text-gray-100 hover:text-primary-600 text-shadow-black hover-margin">
+						<a href="https://www.instagram.com/disnakkeswan.sulsel/" target="_blank" class="text-gray-100 hover:text-primary-600 text-shadow-black hover-margin">
 							<font-awesome-icon icon="fa-brands fa-instagram" />
 						</a>
 						<a href="#" class="text-gray-100 hover:text-primary-600 text-shadow-black hover-margin">
 							<font-awesome-icon icon="fa-brands fa-twitter" />
 						</a>
-						<a href="#" class="text-gray-100 hover:text-primary-600 text-shadow-black hover-margin">
+						<a href="https://wa.me/6282193142389" target="_blank" class="text-gray-100 hover:text-primary-600 text-shadow-black hover-margin">
 							<font-awesome-icon icon="fa-brands fa-whatsapp" />
 						</a>
 					</div>
 					<div class="flex items-center text-gray-100 text-shadow-black">
 						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 -scale-x-100"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" /></svg>
-						<span class="text-sm ml-2">0411 - 123456</span>
+						<span class="text-sm ml-2">+6282193142389</span>
 					</div>
 				</div>
+			</div>
+			<div class="pb-4 pt-16">
+				<p class="text-sm text-gray-200 font-semibold text-center">&copy; Dinas Peternakan dan Kesehatan Hewan Provinsi Sulawesi Selatan 2022. All Rights Reserved.</p>
 			</div>
 		</div>
 	</footer>
